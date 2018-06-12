d01561c3c3d59dd2385bfa494c73f98b70756c35
Colin Tucker <colin@praxis.net.au>
2018-06-12 22:24:31 +1000

    Updated base resources path.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // Define Base:
 
-const BASE = '/resources/silverware/linkedin';
+const BASE = '/resources/vendor/silverware/linkedin';
 
 // Define Paths:
 
